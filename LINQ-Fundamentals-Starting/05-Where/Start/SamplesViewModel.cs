7e998cf8a7b8247c2b30345c35322deae3486921
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2023-05-21 18:40:08 +0200

    Using the Where clause

--- LINQ-Fundamentals-Starting/05-Where/Start/SamplesViewModel.cs
+++ LINQ-Fundamentals-Starting/05-Where/Start/SamplesViewModel.cs
@@ -1,100 +1,105 @@
 ﻿namespace LINQSamples
 {
-  public class SamplesViewModel : ViewModelBase
-  {
-    #region WhereQuery
-    /// <summary>
-    /// Filter products using where. If the data is not found, an empty list is returned
-    /// </summary>
-    public List<Product> WhereQuery()
+    public class SamplesViewModel : ViewModelBase
     {
-      List<Product> products = GetProducts();
-      List<Product> list = new();
+        #region WhereQuery
+        /// <summary>
+        /// Filter products using where. If the data is not found, an empty list is returned
+        /// </summary>
+        public List<Product> WhereQuery()
+        {
+            List<Product> products = GetProducts();
+            List<Product> list;
 
-      // Write Query Syntax Here
+            // Write Query Syntax Here
+            list = (from product in products
+                    where product.Name.StartsWith("S")
+                    select product)
+                    .ToList();
+
+            return list;
+        }
+        #endregion
+
+        #region WhereMethod
+        /// <summary>
+        /// Filter products using where. If the data is not found, an empty list is returned
+        /// </summary>
+        public List<Product> WhereMethod()
+        {
+            List<Product> products = GetProducts();
+            List<Product> list;
+
+            // Write Method Syntax Here
+            list = products
+                .Where(product => product.Name.StartsWith("S"))
+                .ToList();
+
+            return list;
+        }
+        #endregion
+
+        #region WhereTwoFieldsQuery
+        /// <summary>
+        /// Filter products using where with two fields. If the data is not found, an empty list is returned
+        /// </summary>
+        public List<Product> WhereTwoFieldsQuery()
+        {
+            List<Product> products = GetProducts();
+            List<Product> list = new();
+
+            // Write Query Syntax Here
 
 
-      return list;
+            return list;
+        }
+        #endregion
+
+        #region WhereTwoFieldsMethod
+        /// <summary>
+        /// Filter products using where with two fields. If the data is not found, an empty list is returned
+        /// </summary>
+        public List<Product> WhereTwoFieldsMethod()
+        {
+            List<Product> products = GetProducts();
+            List<Product> list = new();
+
+            // Write Method Syntax Here
+
+
+            return list;
+        }
+        #endregion
+
+        #region WhereExtensionQuery
+        /// <summary>
+        /// Filter products using a custom extension method
+        /// </summary>
+        public List<Product> WhereExtensionQuery()
+        {
+            List<Product> products = GetProducts();
+            List<Product> list = new();
+
+            // Write Query Syntax Here
+
+            return list;
+        }
+        #endregion
+
+        #region WhereExtensionMethod
+        /// <summary>
+        /// Filter products using a custom extension method
+        /// </summary>
+        public List<Product> WhereExtensionMethod()
+        {
+            List<Product> products = GetProducts();
+            List<Product> list = new();
+
+            // Write Method Syntax Here
+
+
+            return list;
+        }
+        #endregion
     }
-    #endregion
-
-    #region WhereMethod
-    /// <summary>
-    /// Filter products using where. If the data is not found, an empty list is returned
-    /// </summary>
-    public List<Product> WhereMethod()
-    {
-      List<Product> products = GetProducts();
-      List<Product> list = new();
-
-      // Write Method Syntax Here
-
-
-      return list;
-    }
-    #endregion
-
-    #region WhereTwoFieldsQuery
-    /// <summary>
-    /// Filter products using where with two fields. If the data is not found, an empty list is returned
-    /// </summary>
-    public List<Product> WhereTwoFieldsQuery()
-    {
-      List<Product> products = GetProducts();
-      List<Product> list = new();
-
-      // Write Query Syntax Here
-
-
-      return list;
-    }
-    #endregion
-
-    #region WhereTwoFieldsMethod
-    /// <summary>
-    /// Filter products using where with two fields. If the data is not found, an empty list is returned
-    /// </summary>
-    public List<Product> WhereTwoFieldsMethod()
-    {
-      List<Product> products = GetProducts();
-      List<Product> list = new();
-
-      // Write Method Syntax Here
-
-
-      return list;
-    }
-    #endregion
-
-    #region WhereExtensionQuery
-    /// <summary>
-    /// Filter products using a custom extension method
-    /// </summary>
-    public List<Product> WhereExtensionQuery()
-    {
-      List<Product> products = GetProducts();
-      List<Product> list = new();
-
-      // Write Query Syntax Here
-
-      return list;
-    }
-    #endregion
-
-    #region WhereExtensionMethod
-    /// <summary>
-    /// Filter products using a custom extension method
-    /// </summary>
-    public List<Product> WhereExtensionMethod()
-    {
-      List<Product> products = GetProducts();
-      List<Product> list = new();
-
-      // Write Method Syntax Here
-
-
-      return list;
-    }
-    #endregion
-  }
 }
